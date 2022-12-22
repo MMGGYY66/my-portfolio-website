8da05756d5308cf61e5d67e02e3e4bb6e5cc727a
Javed-Shahbaz <javedshahbaz007@gmail.com>
2022-12-23 02:51:54 +0500

    update script

--- js/formValidate.js
+++ js/formValidate.js
@@ -10,19 +10,3 @@ contactForm.addEventListener('submit', (e) => {
   e.preventDefault();
   
   // Verify the email
-  const emailValue = email.value;
-  if (emailValue.toLowerCase() === emailValue) {
-    errorMessage.innerHTML = '';
-    //  Submit the form
-    contactForm.submit();
-    // // Reset the form
-    contactForm.reset();
-    // Clear the localSotrage
-    localStorage.removeItem('userData');
-  } else {
-    errorMessage.innerHTML = 'Please, enter your email in lower case. ( For example : stephanie@gmail.com)';
-    setTimeout(() => {
-      errorMessage.innerHTML = '';
-    }, 3000);
-  }
-});
