42b8e083c20990fed426cb5a30d85b8464457a3e
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2022-12-22 16:33:56 -0600

    Local storage inside the javascript file

--- js/formValidate.js
+++ js/formValidate.js
@@ -26,3 +26,21 @@ contactForm.addEventListener('submit', (e) => {
     }, 3000);
   }
 });
+
+// ============= LOCAL STORAGE ==================
+
+//  Load data from localStorage
+const loadUserData = () => {
+  try {
+    const data = JSON.parse(localStorage.getItem('userData'));
+    userName.value = data.name;
+    email.value = data.email;
+    message.value = data.message;
+  } catch (error) {
+    const data = { name: '', email: '', message: '' };
+    localStorage.setItem('userData', JSON.stringify(data));
+  }
+};
+
+loadUserData();
+
