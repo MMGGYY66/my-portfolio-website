3da9df12b2d6d479dc985191650e1cff466be358
Javed-Shahbaz <javedshahbaz007@gmail.com>
2022-12-23 02:21:21 +0500

    add email verification to Javascript file

--- js/formValidate.js
+++ js/formValidate.js
@@ -10,4 +10,19 @@ contactForm.addEventListener('submit', (e) => {
   e.preventDefault();
   
   // Verify the email
-  
+  const emailValue = email.value;
+  if (emailValue.toLowerCase() === emailValue) {
+    errorMessage.innerHTML = '';
+    //  Submit the form
+    contactForm.submit();
+    // // Reset the form
+    contactForm.reset();
+    // Clear the localSotrage
+    localStorage.removeItem('userData');
+  } else {
+    errorMessage.innerHTML = 'Please, enter your email in lower case. ( For example : stephanie@gmail.com)';
+    setTimeout(() => {
+      errorMessage.innerHTML = '';
+    }, 3000);
+  }
+});
